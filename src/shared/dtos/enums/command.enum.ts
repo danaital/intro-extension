7b2fd4b7ec986c3ddb7e3d11fc8cc39dcb92bf43
taldanai1995 <taldanai@icloud.com>
2024-02-06 01:12:55 +0200

    Set Base Panel

--- src/shared/dtos/enums/command.enum.ts
+++ src/shared/dtos/enums/command.enum.ts
@@ -2,4 +2,5 @@ export enum CommandEnum {
     HELLO_WORLD = "helloWorld",
     TEST = "test",
     ASK_QUESTION = "askQuestion",
+    OPEN_BASE_PANEL = "openBasePanel",
 }
--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { CommandEnum } from '../shared/dtos/enums/command.enum';
+import { BasePanel } from './base.panel';
 
 const prefix = "intro-extension";
 export interface ICommand {
@@ -8,16 +9,27 @@ export interface ICommand {
     callback: (...args: any[]) => any;
     
 }
-export const commands: ICommand[] = [
-    {command: `${prefix}.${CommandEnum.HELLO_WORLD}`, callback: () => vscode.window.showInformationMessage('Hello World from intro-extension! after refactoring')},
-    {command: `${prefix}.${CommandEnum.TEST}`, callback: () => vscode.window.showInformationMessage('Hello World 2 from intro-extension! after refactoring')},
-    {command: `${prefix}.${CommandEnum.ASK_QUESTION}`, callback: async () => { 
-        const answer = await vscode.window.showInformationMessage('How are you?', 'good', 'bad');
-        vscode.window.showInformationMessage(`You are ${answer}`);
-    }},
-];
-export function registerCommands(context: vscode.ExtensionContext, commands: ICommand[]) {
+export function registerCommands(context: vscode.ExtensionContext) {
+    const commands: ICommand[] = [
+        {command: `${prefix}.${CommandEnum.HELLO_WORLD}`, callback: () => vscode.window.showInformationMessage('Hello World from intro-extension! after refactoring')},
+        {command: `${prefix}.${CommandEnum.TEST}`, callback: () => vscode.window.showInformationMessage('Hello World 2 from intro-extension! after refactoring')},
+        {command: `${prefix}.${CommandEnum.ASK_QUESTION}`, callback: async () => { 
+            const answer = await vscode.window.showInformationMessage('How are you?', 'good', 'bad');
+            vscode.window.showInformationMessage(`You are ${answer}`);
+        }},
+        
+    ];
+    const contextDepCommands = [{command: `${prefix}.${CommandEnum.OPEN_BASE_PANEL}`, callback: () => { 
+        BasePanel.createOrShow(context.extensionUri);
+    }}];
+
+
     for (const command of commands) {
+        const disposable = vscode.commands.registerCommand(command.command, command.callback);
+        context.subscriptions.push(disposable);
+    }
+    
+    for (const command of contextDepCommands) {
         const disposable = vscode.commands.registerCommand(command.command, command.callback);
         context.subscriptions.push(disposable);
     }
