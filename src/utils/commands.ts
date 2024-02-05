6411acb70b06506da4829630837c82a33b43587b
taldanai1995 <taldanai@icloud.com>
2024-02-06 00:42:17 +0200

    refactor interface

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,13 +1,13 @@
 import * as vscode from 'vscode';
 
 const prefix = "intro-extension";
-export interface Command {
+export interface ICommand {
     command: string;
     params?: any[];
     callback: (...args: any[]) => any;
     
 }
-export const commands = [
+export const commands: ICommand[] = [
     {command: `${prefix}.helloWorld`, callback: () => vscode.window.showInformationMessage('Hello World from intro-extension! after refactoring')},
     {command: `${prefix}.test`, callback: () => vscode.window.showInformationMessage('Hello World 2 from intro-extension! after refactoring')},
     {command: `${prefix}.askQuestion`, callback: async () => { 
@@ -15,7 +15,7 @@ export const commands = [
         vscode.window.showInformationMessage(`You are ${answer}`);
     }},
 ];
-export function registerCommands(context: vscode.ExtensionContext, commands: Command[]) {
+export function registerCommands(context: vscode.ExtensionContext, commands: ICommand[]) {
     for (const command of commands) {
         const disposable = vscode.commands.registerCommand(command.command, command.callback);
         context.subscriptions.push(disposable);
