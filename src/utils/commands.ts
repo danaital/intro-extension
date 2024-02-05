5dd32d26d2e53bc8deaf34c4b0f890a63e447aec
taldanai1995 <taldanai@icloud.com>
2024-02-06 00:39:08 +0200

    ask a question

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -10,6 +10,10 @@ export interface Command {
 export const commands = [
     {command: `${prefix}.helloWorld`, callback: () => vscode.window.showInformationMessage('Hello World from intro-extension! after refactoring')},
     {command: `${prefix}.test`, callback: () => vscode.window.showInformationMessage('Hello World 2 from intro-extension! after refactoring')},
+    {command: `${prefix}.askQuestion`, callback: async () => { 
+        const answer = await vscode.window.showInformationMessage('How are you?', 'good', 'bad');
+        vscode.window.showInformationMessage(`You are ${answer}`);
+    }},
 ];
 export function registerCommands(context: vscode.ExtensionContext, commands: Command[]) {
     for (const command of commands) {
